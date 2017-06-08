1264d4e2eb389c52a56a54f26b675a7f30367769
github username <github email address>
2017-06-08 17:44:04 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,52 @@
-# code here!
+# require 'pry'
+
+class School
+
+def initialize(school_name)
+  @school_name = school_name
+  @roster = {}
+end
+
+
+def roster
+  @roster
+end
+
+
+def add_student(student,grade)
+
+@roster[grade] = [] if !@roster.include?(grade)
+   @roster[grade] << student
+
+end
+
+def grade(grade)
+
+@roster[grade]
+
+end
+
+
+def sort
+   @roster.each do |grade,students|
+     @roster[grade].sort!
+  end
+end
+
+
+
+
+end
+
+
+# Pry.start
+
+
+
+
+
+
+
+# school.map do |grade, student|
+#    p school[grade].sort
+# end
